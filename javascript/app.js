925cdac3ee2fe65e043e8c33157cf5d6c07e4640
unknown <stanmozolevskiy@gmail.com>
2018-11-17 22:22:20 -0500

    dgrfg

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    $(".dropdown-trigger").dropdown();
+
+    $(".sidenav").sidenav();
+
+    $('.parallax').parallax();
+  
+    $('.carousel').carousel();
+
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 100
+    });
+
+});
